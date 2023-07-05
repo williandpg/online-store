a512ca42e1ffd3aba741c3e1a9f8ad8dd3665b3d
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-05 16:24:41 -0300

    inplementação da api.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as api from './services/api';
 import './App.css';
+
+api.getCategories().then((categories) => { console.log(categories); });
+api.getProductsFromCategoryAndQuery('MLB271599', 'Agro').then((a) => { console.log(a); });
+api.getProductById('MLB3174626530').then((product) => { console.log(product); });
 
 function App() {
   return (
