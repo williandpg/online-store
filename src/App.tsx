fc487e5c3be16f5ba2f551913213526bcb2c0388
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-05 17:23:58 -0300

    Pagina Home criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+// Api
 import * as api from './services/api';
+
+// Pages
+import Home from './pages/home';
+
+// Css
 import './App.css';
 
-api.getCategories().then((categories) => { console.log(categories); });
-api.getProductsFromCategoryAndQuery('MLB271599', 'Agro').then((a) => { console.log(a); });
-api.getProductById('MLB3174626530').then((product) => { console.log(product); });
+// api.getCategories().then((categories) => { console.log(categories); });
+// api.getProductsFromCategoryAndQuery('MLB271599', 'Agro').then((a) => { console.log(a); });
+// api.getProductById('MLB3174626530').then((product) => { console.log(product); });
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
---
+++ src/pages/home.tsx
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <>
+      <form>
+        <input type="text" name="" id="" />
+        <button type="submit">Enviar</button>
+      </form>
+
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+
+      <div id="Produtos" />
+    </>
+  );
+}
+
+export default Home;
