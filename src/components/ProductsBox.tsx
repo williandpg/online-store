d54ff62dcddef7cd35cdbfcc491d4c510ed7dc53
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-05 18:43:54 -0300

    Correção de lint

--- src/components/ProductsBox.tsx
+++ src/components/ProductsBox.tsx
@@ -1,10 +1,10 @@
 function ProductBox(productName: string, productImage: string, productPrice: string) {
   return (
-      <button>
-        <img src={productImage} alt={`imagem de ${productName}`} />
-        <h2>{productPrice}</h2>
-        <p>{productName}</p>
-      </button>
+    <button>
+      <img src={ productImage } alt={ `imagem de ${productName}` } />
+      <h2>{ productPrice }</h2>
+      <p>{ productName }</p>
+    </button>
   );
 }
 
