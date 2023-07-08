d221c5fdbb7e21bbbede104c748c32b336744d1f
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-08 16:05:21 -0300

    correction lint

--- src/pages/Carrinho.tsx
+++ src/pages/Carrinho.tsx
@@ -1,5 +1,4 @@
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Carrinho() {
   const navigate = useNavigate();
@@ -18,13 +17,28 @@ function Carrinho() {
         </div>
       ) : (
         <section>
-          {cart.map((item: { thumbnail: string | undefined; title: string; quantity: number | null | undefined; price: number }) => (
-           <div> 
-            <img src={item.thumbnail} />
-            <h3 data-testid="shopping-cart-product-name">{item.title} </h3>
-            <h3 data-testid="shopping-cart-product-quantity">Quantidade: {item.quantity}</h3>
-            <span>Valor do item: {item.price}</span>
-           </div> 
+          {cart.map((item: {
+            thumbnail: string | undefined;
+            title: string;
+            quantity: number | null | undefined;
+            price: number }) => (
+              <div key={ item.title }>
+                <img src={ item.thumbnail } alt={ `Imagem de ${item.title}` } />
+                <h3 data-testid="shopping-cart-product-name">
+                  {item.title}
+                  {' '}
+                </h3>
+                <h3 data-testid="shopping-cart-product-quantity">
+                  Quantidade:
+                  {' '}
+                  {item.quantity}
+                </h3>
+                <span>
+                  Valor do item:
+                  {' '}
+                  {item.price}
+                </span>
+              </div>
           ))}
         </section>
       )}
